479d7ad8fb4c1857bc943b621e9d2460d5589a50
Juan Aguete_Regueira <jagueregu@cdm.local>
2026-09-18 10:43:39 +0200

    +EjemplosConversionesTipos

---
+++ ProyectosClase/ud1/EjemplosConversionesTipos.java
@@ -0,0 +1,26 @@
+package ud1;
+
+public class EjemplosConversionesTipos {
+    public static void main(String[] args) {
+        
+        double a = 56;
+        System.out.println(a);
+
+        int b = (int)5.6; /*La expresión de la derecha la asignas al tipo de datos que 
+        pones entre parentesis. Es una conversión de tipo explicita o de estrechamiento
+        ya que como puedes ver se pierden datos.*/
+        System.out.println(b);
+
+        float c = 923546923444444443l;
+        System.out.println(c);
+
+        int num = Integer.parseInt("9823462"); // Con datos tipo int
+        System.out.println(num + 5);
+
+        double num2 = Double.parseDouble("3786548.34");
+        System.out.println(num2);
+
+
+    }
+
+}
