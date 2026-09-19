20f0558935e7f9e86914cf693e0229219ae1f8d6
juansxs <agueteregueirajuan@gmail.com>
2026-09-19 18:05:49 +0200

    Par file add

---
+++ PrimerProyecto/src/ud1/Par.java
@@ -0,0 +1,33 @@
+package ud1;
+
+import java.util.Scanner;
+
+public class Par {
+    public static void main(String[] args) {
+        
+        //Entrada
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce un número para evaluarlo: ");
+        int numero = sc.nextInt();
+        sc.close();
+
+        //Proceso
+        int operacion = numero % 2;
+        boolean resultado = operacion == 0;
+
+        //Salida
+        System.out.println("Era tu número par ? --> " + resultado);
+        
+        
+
+
+
+
+
+
+
+
+
+    }
+
+}
